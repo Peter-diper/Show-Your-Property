db608731d8192727685611a3dd27f460986a7c14
peter-diper <rasoolfada2@gmail.com>
2026-08-10 13:49:16 +0330

    feat: refactor MessageCount component; improve loading state handling and UI presentation

--- components/MessageCount.jsx
+++ components/MessageCount.jsx
@@ -1,10 +1,9 @@
 import { useGlobalStore } from "@/store/store";
 import React, { useEffect, useState } from "react";
 
-const MessageCount = () => {
+const MessageCount = ({}) => {
   const { unReadcount, setReadcount } = useGlobalStore();
 
-  const [unreadMessages, setUnreadMessages] = useState(null);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -15,9 +14,7 @@ const MessageCount = () => {
 
         if (res.status === 200) {
           const data = await res.json();
-          console.log(unReadcount);
           setReadcount(data);
-          console.log(unReadcount);
         }
       } catch (error) {
         console.log(error);
@@ -26,15 +23,20 @@ const MessageCount = () => {
       }
     };
     fetchUnreadMessages();
-  }, []);
+  }, [setReadcount]);
 
   return (
-    unreadMessages !== 0 && (
-      <span
-        className={`absolute -top-1 -right-1 w-4 h-4 flex items-center justify-center text-[10px] font-bold text-white ${loading ? "bg-gray-500 animate-pulse " : "bg-red-500"} rounded-full`}
-      >
-        {loading ? ".." : unReadcount}
-      </span>
+    unReadcount !== 0 && (
+      <>
+        <span
+          className={`absolute -top-1 -right-1 w-4 h-4 flex items-center justify-center text-[10px] font-bold bg-white/5 border  border-red-500 scale-125 animate-ping rounded-full`}
+        ></span>
+        <span
+          className={`absolute -top-1 -right-1 w-4 h-4 flex items-center justify-center text-[10px] font-bold size-4 overflow-hidden text-white ${loading ? "bg-gray-500 animate-pulse " : "bg-red-500"} rounded-full`}
+        >
+          {loading ? ".." : unReadcount}
+        </span>
+      </>
     )
   );
 };
