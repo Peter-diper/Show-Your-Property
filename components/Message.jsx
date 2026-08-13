8ad5987432d72a584bb172a1268c831b03cc3c7e
peter-diper <rasoolfada2@gmail.com>
2026-08-14 00:18:30 +0330

    feat: disable buttons during loading states in Message component for better user experience

--- components/Message.jsx
+++ components/Message.jsx
@@ -127,6 +127,7 @@ const Message = ({ message }) => {
 
       <div className="flex justify-end gap-3 mt-8">
         <button
+          disabled={loading}
           onClick={handleReadClick}
           className={`px-5 py-2 rounded-lg ${loading ? "bg-slate-100 text-slate-700 hover:bg-slate-200 animate-pulse" : isRead ? "bg-green-100 text-green-700 hover:bg-green-200" : "bg-blue-100 text-blue-700 hover:bg-blue-200"} transition`}
         >
@@ -134,6 +135,7 @@ const Message = ({ message }) => {
         </button>
 
         <button
+          disabled={loadingDelete}
           onClick={handleDelete}
           className={`px-5 py-2 rounded-lg text-white ${loadingDelete ? "bg-gray-500  hover:bg-gray-600 animate-pulse" : "bg-red-500  hover:bg-red-600"} transition`}
         >
